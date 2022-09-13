56696187b5b399ee0e54c4f79c8887e4221f3315
zongxin1993 <36766561+zongxin1993@users.noreply.github.com>
2022-09-13 18:58:39 +0800

    Update zsh.sh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -2,14 +2,12 @@
 
 sudo apt install zsh -y
 
-set USER=zongxin
-set HOME=/home/zongxin
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"  --skip-chsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh}/plugins/zsh-autosuggestions
 
-sed -i '73d' /home/zongxin/.zshrc
+sed -i '73d' ~/.zshrc
 sed -i '73i\plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' ~/.zshrc
 
-sed -i 's/%c%/[\$PWD]%/' /home/zongxin/.oh-my-zsh/themes/robbyrussell.zsh-theme
+sed -i 's/%c%/[\$PWD]%/' ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
