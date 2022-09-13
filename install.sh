dc3e769de4b2837bfef625da4d1d0528394f4fd9
Zong Xin <zongxin19930430@163.com>
2022-09-13 17:25:36 +0800

    Initial commit

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 
 while [ $# -gt 0 ]; do
   case $1 in
-    --base-env) bash scripts/bash-env.sh ;;
+    --base-env) bash scripts/base-env.sh ;;
     --docker) bash scripts/docker.sh ;;
     --nvim) bash scripts/nvim.sh ;;
     --tmux) bash scripts/tmux.sh ;;
