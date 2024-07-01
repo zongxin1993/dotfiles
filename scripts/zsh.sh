1afafb819a2342d380ac0d12fc08c87e9e8105ae
Zong,xin <zongxin19930430@163.com>
2024-07-01 13:56:19 +0800

    update

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -3,11 +3,7 @@
 sudo apt install zsh -y
 
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh}/plugins/zsh-autosuggestions
 
-sed -i '73d' ~/.zshrc
-sed -i '73i\plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' ~/.zshrc
-
-sed -i 's/%c%/[\$PWD]%/' ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
