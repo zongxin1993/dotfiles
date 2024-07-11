46640957777afc8f1393ce4bdc98545ff2702b67
xin zong <36766561+zongxin1993@users.noreply.github.com>
2024-07-11 16:07:36 +0800

    Update nvim.sh

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -16,11 +16,11 @@ sudo apt-get install -y \
   xsel \
   ripgrep
 
-git clone https://github.com/neovim/neovim.git --depth 1 -b v0.9.0 && \
+git clone https://github.com/neovim/neovim.git --depth 1 -b v0.10.0 && \
 cd neovim && \
 make CMAKE_BUILD_TYPE=RelWithDebInfo -j && \
 sudo make install 
 
 pip3 install neovim
 
-git clone https://github.com/zongxin1993/nvim-config.git /home/zongxin/.config/nvim --depth 1
+git clone https://github.com/zongxin1993/zvim.git /home/zongxin/.config/nvim --depth 1
