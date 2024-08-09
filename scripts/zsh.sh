9ac169e636930eaff93dc1731edbc820b5134d05
xin zong <36766561+zongxin1993@users.noreply.github.com>
2024-08-09 10:34:11 +0800

    Update zsh.sh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -2,8 +2,7 @@
 
 sudo apt install zsh -y
 
-
-sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh}/plugins/zsh-autosuggestions
 
+sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
